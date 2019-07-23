d7f1f706098ba2ed1655317852e387ddd6b30d19
DKOKING <kristian.ofori@csedge.org>
2019-07-23 11:57:00 -0700

    Updated Main.Py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import webapp2
 import jinja2
 import os
+import random
 
 # this initializes the jinja2 environment
 # this will be the same in every app that uses the jinja2 templating library 
@@ -11,11 +12,56 @@ the_jinja_env = jinja2.Environment(
   autoescape=True)
 
 # other functions should go above the handlers or in a separate file
+def getPersonality(clothes1, clothes2):
+	if clothes1 == "":
+		print('ye')
 
+	elif clothes1 == "":
+		print('ye')
+
+	elif clothes1 == "":
+		print('ye')
+
+	elif clothes1 == "":
+		print('ye')
+
+	elif clothes1 == "":
+		print('ye')
+
+	elif clothes1 == "":
+		print('ye')
+		
+	if clothes2 == "":
+		print('ye')
+	elif clothes2 == "":
+		print('ye')
+	elif clothes2 == "":
+		print('ye')
+	elif clothes2 == "":
+		print('ye')
+	elif clothes2 == "":
+		print('ye')
+	elif clothes2 == "":
+		print('ye')
 # the handler section
 class MainHandler(webapp2.RequestHandler):
-  def get(self):  # for a get request
-    self.response.write('Greetings!')  # the response
+  	def get(self):  # for a get request
+		self.response.write('Greetings')  # the response
+
+class AvatarHandler(object):
+	def get(self):
+		print('Hello!')
+
+	def post(self):
+		prediction_template = JINJA_ENVIRONMENT.get_template('templates/fortune.html')
+		print('Hello')
+		"""avatar = self.request.get('userAvatar')
+		personality = getPersonality(self.request.get(''))
+		avatar = {
+		"personality": personality
+		"accuracy": random.randint(0, 100)
+		}
+		self.response.write(welcome_template.render(avatar))"""
 
 
 # the app configuration section	
